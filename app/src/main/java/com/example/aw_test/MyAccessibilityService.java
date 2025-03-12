7da830f3b5a8881136e0773e15da631b36cd87fa
Testor807 <testor807@gmail.com>
2025-03-12 22:18:58 +0800

    upload

--- app/src/main/java/com/example/aw_test/MyAccessibilityService.java
+++ app/src/main/java/com/example/aw_test/MyAccessibilityService.java
@@ -1,8 +1,8 @@
 package com.example.aw_test;
 
 import android.accessibilityservice.AccessibilityService;
-import android.accessibilityservice.AccessibilityServiceInfo;
-import android.os.Bundle;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -36,59 +36,35 @@ public class MyAccessibilityService extends AccessibilityService {
             // 其他事件类型...
         }
     }
-    public void onAccessibilityEvent2(AccessibilityEvent event) {
-        // 处理可访问性事件
-        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
-            Log.d("AccessibilityService","Page Changed!");
-            String packageName = event.getPackageName().toString();
-            if (packageName.equals("com.google.android.youtube")) {
-                Log.d("AccessibilityService","Youtube Searching");
-                performSearch("Appium");
-            }
-        }
-    }
-
-    private void performSearch(String query) {
-        System.out.println("Youtube Windows");
-        AccessibilityNodeInfo rootNode = getRootInActiveWindow();
-        if (rootNode != null) {
-            // 查找搜索框并输入关键字
-            List<AccessibilityNodeInfo> searchBoxes = rootNode.findAccessibilityNodeInfosByViewId("com.google.android.youtube:id/search_box");
-            if (!searchBoxes.isEmpty()) {
-                AccessibilityNodeInfo searchBox = searchBoxes.get(0);
-                searchBox.performAction(AccessibilityNodeInfo.ACTION_FOCUS);
-                Bundle arguments = new Bundle();
-                arguments.putCharSequence(AccessibilityNodeInfo.ACTION_ARGUMENT_SET_TEXT_CHARSEQUENCE, query);
-                searchBox.performAction(AccessibilityNodeInfo.ACTION_SET_TEXT, arguments);
-
-                // 查找并点击搜索按钮
-                List<AccessibilityNodeInfo> searchButtons = rootNode.findAccessibilityNodeInfosByViewId("com.google.android.youtube:id/search_button");
-                if (!searchButtons.isEmpty()) {
-                    searchButtons.get(0).performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                }
-            }
-        } else {
-            Log.e("AccessibilityService", "Root node is null");
-        }
-    }
 
     @Override
     public void onInterrupt() {
-        Log.d(TAG, "onInterrupt: Service Interrupted");
+
     }
 
-    @Override
-    public void onServiceConnected() {
-        super.onServiceConnected();
-        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED | AccessibilityEvent.TYPE_VIEW_FOCUSED;
-        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
-        info.notificationTimeout = 100;
-        info.packageNames = new String[]{"com.example.android.youtube"};
-        info.flags = AccessibilityServiceInfo.FLAG_INCLUDE_NOT_IMPORTANT_VIEWS;
-        setServiceInfo(info);
-        Log.d(TAG,"Service Started");
-        //youtube = new Youtube();
+    public void getCurrentPage(AccessibilityEvent event) {
+        AccessibilityNodeInfo source = event.getSource();
+
+        if (source != null) {
+            CharSequence packageName = source.getPackageName();
+            if (event.getEventType() == event.TYPE_WINDOW_STATE_CHANGED) {
+
+                //获取当前窗口activity名
+                ComponentName componentName = new ComponentName(
+                        event.getPackageName().toString(),
+                        event.getClassName().toString()
+                );
+                try {
+                    String activityName = getPackageManager().getActivityInfo(componentName, 0).toString();
+                    activityName = activityName.substring(activityName.indexOf(" "), activityName.indexOf("}"));
+                    Log.e("当前窗口activity", "=================" + activityName);
+                } catch (PackageManager.NameNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
+
+
 
 }
